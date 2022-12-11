ebb0628f9862a95f88508ef4dda168e52f7ccefc
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-12-11 13:32:46 -0300

    atualizando exemplos

--- covid19_tracker/geolocation.py
+++ covid19_tracker/geolocation.py
@@ -1,9 +1,31 @@
-import random
+from urllib.request import urlopen
 from geopy.geocoders import Nominatim
 import geocoder
 import csv
 
+#tem 2 funcoes pq existe um limite de requisições que podem ser feitas por dia em cada API
+#Então caso atinja o limite, basta utilizar a outra função
 def get_location(ip):
+
+    if ip:
+        #url da api
+        url = f"http://ip-api.com/json/{ip}"
+        
+        request = urlopen(url)
+        data = request.read().decode()
+        
+        data = eval(data)
+    if data['status'] != "success": 
+        myAddress = []
+        return myAddress
+    myAddress = []
+    myAddress.append(data['lat'])
+    myAddress.append(data['lon'])
+    
+    return myAddress
+
+
+def get_location2(ip):
 
     f = geocoder.ip(ip)
 
