81a65df4f339d2466143159b6ac01a251d9d2fbd
Strovertz - Gleison Pires <74078237+strovertz@users.noreply.github.com>
2022-12-12 12:02:15 -0300

    atualizando

--- covid19_tracker/main.py
+++ covid19_tracker/main.py
@@ -1,24 +1,34 @@
 from geolocation import *
 from person import *
 import folium
+import numpy
 
+#dicionarios é uma lista que contem todos os dicionarios, ou melhor, os usuários. 
 coord_geo = []
 dicionarios = []
 dicionario = {}
 
 def set_markup(mapa, location, user):
-    popupe = "User" + str(user.user) + "\n" + "Nome: " + str(user.name) + "\n Born: " + str(user.birthday) + "\n Covid Status: " + str(user.covid) + "\n Vaccination Status: " + str(user.vaccination)
-    folium.Marker([location[0], location[1]], popup = popupe).add_to(mapa)
-    return mapa   
+    popupe = "User: " + str(user.user) + "\n" + "Nome: " + str(user.name) + "\n Born: " + str(user.birthday) + "\n Covid Status: " + str(user.covid) + "\n Vaccination Status: " + str(user.vaccination)
+    if str(user.covid) == "Positive":
+        icon = folium.features.CustomIcon("icon/Biohazard.png",
+                                      icon_size=(14, 14))
+        folium.Marker([location[0], location[1]], popup = popupe, icon=icon, icon_size=(30,30)).add_to(mapa)
+        folium.CircleMarker([location[0], location[1]], radius=50, popup = "Infected Area", color = "red", fill = True, fill_color="lightred").add_to(mapa)
+        return mapa
+    else:
+        folium.Marker([location[0], location[1]], popup = popupe, icon=folium.Icon(color='lightblue')).add_to(mapa)
+        return mapa   
     
 def adicionar_dicionario(dicionario):
-  # Adiciona o dicionário à lista "dicionarios"
+  #Adiciona o dicionário à lista "dicionarios"
   dicionarios.append(dicionario)
   
 def create_map(my_location):
+    #Retorna um mapa com a minha localização
     mapa = folium.Map(location=[my_location[0], my_location[1]], zoom_start=1)
     popupe = "User: Strovertz" + "\n" + "Nome: Gleison Pires" + "\n Born: 2002-12-04" + "\n Covid Status: Negative" + "\n Vaccination Status: 3 Doses"
-    folium.Marker([my_location[0], my_location[1]], popup = popupe).add_to(mapa)
+    folium.Marker([my_location[0], my_location[1]], popup = popupe, icon=folium.Icon(color='lightgreen', icon='home')).add_to(mapa)
     return mapa      
 
 def print_test(lat_lon):
@@ -43,7 +53,7 @@ def user_create(lat_lon, ip, i):
 #As api de consulta tem um limite de consulta diario, entao para evitar alguns problemas, vamos utilizar
 #2 apis diferentes para fazermos um "reverse" a partir do IP fornecido e encontrarmos as coordenadas do nosso "usuario encovidado"
 def load_balancer(ip):
-    opt  = ["0","1"]
+    opt  = ["1"]
     function = random.choice(opt)
     loc = []
     if function:
@@ -55,35 +65,49 @@ def load_balancer(ip):
 def print_matrix(adjacency_matrix):
     linhas = len(adjacency_matrix)
     colunas = len(adjacency_matrix[0])
-
+    #vou salvar apenas pra caso precise consultar algum específico pra verificar a as adjacencias, 
+    #quando eu  aplicar uma matriz 1000x1000 vai ficar mais dificil de visualizar pelo prompt
+    file = open("data/adjacency_matrix.txt", "w+")
     for i in range(linhas):
         for j in range(colunas):
             if(j == colunas - 1):
                 print("%d" %adjacency_matrix[i][j])
+                file.write(str(adjacency_matrix[i][j]))
             else:
                 print("%d" %adjacency_matrix[i][j], end = " ")
+                file.write(str(adjacency_matrix[i][j]))
+        file.write('\n')
+        
     print()
         
+def dict_list_dump():
+    with open("data/json/users/all_usr.json", "w", encoding ='utf8') as outfile:
+        json.dump(dicionarios, outfile, indent=4, sort_keys=True)
+        outfile.close()
 
-def main():
+def matrix_actions():
+    adjacency_matrix = matrix_create()
+    print_matrix(adjacency_matrix)    
+    
+def first_action():
     my_location = [-29.6894956, -53.811126]
-    for i in range(10):
+    mapa = create_map(my_location)
+
+    for i in range(1000):
         ip = rand_ipAddress()
         lat_lon = load_balancer(ip)
-        mapa = create_map(my_location)
         if len(lat_lon) < 1: continue
-        
         #Esses prints tão só pra ter algo pra ver no terminal enquanto processa os dados kkk
         print_test(lat_lon)
-        
         user = user_create(lat_lon, ip, i)
         mapa = set_markup(mapa, lat_lon, user)
-        
-    adjacency_matrix = matrix_create()
-    print_matrix(adjacency_matrix)  
-    #print(dicionarios)
-
     mapa.save("map/my_map1.html")
+    matrix_actions()
+    #return mapa
+
+def main():
+    first_action()
+    dict_list_dump()
     
 if __name__ == '__main__': 
     main()
